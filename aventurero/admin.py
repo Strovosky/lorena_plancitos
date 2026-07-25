594eaad5b5391a23a6ffc7c08a87a4d4a4665590
strovosky <strovosky@gmail.com>
2026-07-25 11:48:51 -0500

    Modified the admin Login form because it wasn't working

--- aventurero/admin.py
+++ aventurero/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Aventurero
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class AventureroAdmin(UserAdmin):
+    model = Aventurero
+    list_display = ("correo", "usuario", "is_staff", "is_active",)
+    list_filter = ("is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("correo", "password")}),
+        ("Personal info", {"fields": ("usuario", "nombre", "apellido", "telefono")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("correo", "usuario", "nombre", "apellido", "telefono", "password1", "password2", "is_staff", "is_active", "is_superuser"),
+        }),
+    )
+    search_fields = ("correo",)
+    ordering = ("correo",)
 
 admin.site.register(Aventurero)
