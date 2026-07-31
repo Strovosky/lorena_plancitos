939a6401c2f87b63c0d69c3b0d3017745092eed8
strovosky <strovosky@gmail.com>
2026-07-31 11:50:39 -0500

    Termine procesamiento de imagenes de Experiencias

--- experiencias/models.py
+++ experiencias/models.py
@@ -1,16 +1,28 @@
 from django.db.models import Model, CharField, DateTimeField, TextField, ImageField
 from django.utils.translation import gettext_lazy as _
+from django.core.files.base import ContentFile
+from .validators import validar_tamaño_archivo
 from PIL import Image
-
-# Create your models here.
+import os
+from io import BytesIO
+from .validators import (
+    num_peso_max,
+    peso_maximo_if_ok,
+    peso_minimo_if_ok,
+    foto_alto_min,
+    foto_ancho_min,
+    dimensiones_min,
+    validar_min_dimension,
+    validar_max_dimension
+    )
 
 class Experiencia(Model):
     """Este modelo es el diseño para los planes que se ofreceran a la gente"""
 
     OPCIONES_ESTADOS = [
-        ('ACTIVO', 'activo'),
-        ('FINALIZADO', 'finalizado'),
-        ('CANCELADO', 'cancelado')
+        ('activo', 'Activo'),
+        ('finalizado', 'Finalizado'),
+        ('cancelado', 'Cancelado')
     ]
 
     titulo = CharField(_('título'), max_length=150)
@@ -21,20 +33,66 @@ class Experiencia(Model):
     ciudad = CharField(_('ciudad'), max_length=100)
     direccion = CharField(_('dirección'), max_length=200, blank=True, null=True)
     establecimiento = CharField(_('establecimiento'), max_length=150, blank=True, null=True)
-    foto = ImageField(_('foto'), default='experiencia_generica.jpg', upload_to='imagenes_experiencias')
+    foto = ImageField(
+        _('foto'),
+        default='experiencia_generica.webp',
+        upload_to='imagenes_experiencias',
+        validators=[validar_tamaño_archivo, validar_min_dimension, validar_max_dimension],
+        help_text=_(f'El archivo no puede pesar más de {num_peso_max}.')
+        )
 
     def __str__(self):
         return f'{self.__class__.__name__}: {self.id} - {self.titulo}'
-
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        imagen = Image.open(self.foto.path)
+        if self.foto and self.foto.name != 'experiencia_generica.webp': # Si la foto existe y no es la generica
+            try:
+                imagen = Image.open(self.foto.path)
+                necesita_procesar = False
+                quality = 100
 
-        if imagen.width > 800 and imagen.height > 500:
-            max_dimensiones = (800,500) #(width, height)
-            imagen.thumbnail(max_dimensiones)
-            imagen.save(self.foto.path)
+                # Si la foto es mas grande de lo necesario, procesala a dimensiones aceptables maximas
+                if imagen.width > foto_ancho_min or imagen.height > foto_alto_min:
+                    necesita_procesar = True
+
+                # Si la foto pesa mucho a pesar de estar en dimensiones ideales, procesala para bajarle calidad y peso
+                if self.foto.size > peso_minimo_if_ok:
+                    if self.foto.size > peso_maximo_if_ok:
+                        quality = 75
+                    elif self.foto.size > peso_minimo_if_ok:
+                        quality = 85
+                    else:
+                        quality = 90
+                    necesita_procesar = True
+
+                # Si la imagen es un cuadrado, queremos volverlo 
+                if self.foto.width == self.foto.height:
+                    imagen = imagen.resize(dimensiones_min, Image.Resampling.LANCZOS)
+                    
+                if necesita_procesar:
+                    imagen.thumbnail(dimensiones_min, Image.Resampling.LANCZOS) #dimensiones_min es (1600*1000)
+
+                    buffer = BytesIO()
+                    imagen.save(buffer, format='WEBP', quality=quality)
+                    buffer.seek(0)
+
+                    # Obtengamos el nombre sin el path
+                    # Porque self.foto.name me da el nombre con el path 'experiencias/la_foto.webp'
+                    nombre_base = os.path.basename(self.foto.name)
+                    nombre, ext = os.path.splitext(nombre_base)
+
+                    # Si la foto vieja que se haya subido aun existe, borrala
+                    path_foto_vieja = self.foto.path
+
+                    self.foto.save(content=ContentFile(buffer.read()), name=f'{nombre}.webp', save=False)
+                    super().save(*args, **kwargs)
+
+                    if ext.lower() != '.webp' or path_foto_vieja != self.foto.path:
+                        os.remove(path_foto_vieja)
+                                 
+            except Exception as e:
+                RuntimeError(_(f'Hubo un error al procesar la foto de la experiencia {self.id}'))
 
 
---
+++ experiencias/validators.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ValidationError
+
+# Las dimensiones minimas y maximas de las fotos
+foto_ancho_min, foto_alto_min, foto_ancho_max, foto_alto_max = 1600, 900, 4096, 4096
+dimensiones_max = (foto_ancho_max,foto_alto_max) #(width, height)
+dimensiones_min = (foto_ancho_min, foto_alto_min)
+
+# El peso máximo de una foto, si se trata de subir por encima de este, el validador no te dejará.
+num_peso_max = 3
+peso_maximo = num_peso_max * 1024 * 1024 # El num_peso_max ens MB in bytes
+
+# Si la imagen esta dentro de las dimensiones deseadas, que peso es valido que tenga
+# peso_minimo_if_ok es el peso para redimensionar la imagen si se pasa de esto. quiality 85%
+# peso_maximo_if_ok es el peso para redimensionar la imagen si se pasa de esto, pero quality 75%
+peso_minimo_if_ok = 500 * 1024 # 500 KB
+peso_maximo_if_ok = 1.5 * 1024 * 1024
+
+def validar_tamaño_archivo(valor):
+    if valor.size > peso_maximo:
+        raise ValidationError(f'El archivo no puede exceder las {num_peso_max}MB.')
+
+def validar_max_dimension(valor):
+    if valor.width > foto_ancho_max and valor.height > foto_alto_max:
+        raise ValidationError(f'Las dimensiones del archivo son muy grandes. El maximo es ({dimensiones_max}) y la imagen tiene ({valor.width}, {valor.height})')
+    elif valor.height > foto_alto_max:
+        raise ValidationError(f'El ancho de la imagen esta bien pero esta muy alta. El máximo es {foto_alto_max} y la imagen tiene {valor.height}')
+    elif valor.width > foto_ancho_max:
+        raise ValidationError(f'El alto de la imagen esta bien pero esta muy ancha. El máximo es {foto_ancho_max} y la imagen tiene {valor.width}')
+
+
+def validar_min_dimension(valor):
+    if valor.width < foto_ancho_min and valor.height < foto_alto_min:
+        raise ValidationError(f'Las dimensiones del archivo son muy pequeñas. El minimo es ({dimensiones_min}) y la imagen tiene ({valor.width}, {valor.height})')
+    elif valor.height < foto_alto_min:
+        raise ValidationError(f'El ancho de la imagen esta bien pero esta muy baja. El minimo es {foto_alto_min} y la imagen tiene {valor.height}')
+    elif valor.width < foto_ancho_min:
+        raise ValidationError(f'El alto de la imagen esta bien pero esta muy angosta. El minimo es {foto_ancho_min} y la imagen tiene {valor.width}')
